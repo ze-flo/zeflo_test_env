e50f219154033bf1d315828add2044e3e5a64fca
Florent <florent.mathieu@gmail.com>
2021-03-04 01:58:08 -0500

    feat(utils): add `divide` function
    cool for dividing numbers

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ function multiply(a, b) {
     return a * b
 }
 
+function divide(a = 0, b = 0) {
+    if (b === 0) throw new Error("can't divide by 0")
+
+    return a / b
+}
+
 module.exports = {
     add,
     multiply,
+    divide,
 }
